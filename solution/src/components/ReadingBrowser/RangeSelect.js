fc9bc37573e69249d660610b8a949a92f5e7a5c5
christianLB <k2600x@gmail.com>
2019-01-06 11:45:23 +0100

    implemented material-ui core, icons, styles. - select time range component migrated to material-ui Select

--- solution/src/components/ReadingBrowser/RangeSelect.js
+++ solution/src/components/ReadingBrowser/RangeSelect.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import Select from '@material-ui/core/Select';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
 import moment from 'moment';
 
 
@@ -8,6 +13,8 @@ class RangeSelect extends Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
+        this.state = {selectedValue: 5};
+
     }
 
     handleChange(e) {
@@ -16,31 +23,68 @@ class RangeSelect extends Component {
         };
         range.start = moment().subtract(e.target.value, 'hours');
         this.props.onChange(range);
+        this.setState({selectedValue: e.target.value});
     }
 
     render() {
-        return <select onChange={this.handleChange}>
-            <option defaultValue value="5">Last 5 hours</option>
-            <option value="6">Last 6 hours</option>
-            <option value="7">Last 7 hours</option>
-            <option value="8">Last 8 hours</option>
-            <option value="9">Last 9 hours</option>
-            <option value="10">Last 10 hours</option>
-            <option value="11">Last 11 hours</option>
-            <option value="12">Last 12 hours</option>
-            <option value="13">Last 13 hours</option>
-            <option value="14">Last 14 hours</option>
-            <option value="15">Last 15 hours</option>
-            <option value="16">Last 16 hours</option>
-            <option value="17">Last 17 hours</option>
-            <option value="18">Last 18 hours</option>
-            <option value="19">Last 19 hours</option>
-            <option value="20">Last 20 hours</option>
-            <option value="21">Last 21 hours</option>
-            <option value="22">Last 22 hours</option>
-            <option value="23">Last 23 hours</option>
-            <option value="24">Last 24 hours</option>
-        </select>;
+        const { selectedValue } = this.state;
+        const { handleChange } = this;
+
+        return <FormControl>
+            <InputLabel htmlFor="time-helper">Time Range</InputLabel>
+            <Select
+                value={selectedValue}
+                onChange={handleChange}
+                input={<Input name="time" id="time-helper" />}
+            >
+                <MenuItem value="5">Last 5 hours</MenuItem>
+                <MenuItem value="6">Last 6 hours</MenuItem>
+                <MenuItem value="7">Last 7 hours</MenuItem>
+                <MenuItem value="8">Last 8 hours</MenuItem>
+                <MenuItem value="9">Last 9 hours</MenuItem>
+                <MenuItem value="10">Last 10 hours</MenuItem>
+                <MenuItem value="11">Last 11 hours</MenuItem>
+                <MenuItem value="12">Last 12 hours</MenuItem>
+                <MenuItem value="13">Last 13 hours</MenuItem>
+                <MenuItem value="14">Last 14 hours</MenuItem>
+                <MenuItem value="15">Last 15 hours</MenuItem>
+                <MenuItem value="16">Last 16 hours</MenuItem>
+                <MenuItem value="17">Last 17 hours</MenuItem>
+                <MenuItem value="18">Last 18 hours</MenuItem>
+                <MenuItem value="19">Last 19 hours</MenuItem>
+                <MenuItem value="20">Last 20 hours</MenuItem>
+                <MenuItem value="21">Last 21 hours</MenuItem>
+                <MenuItem value="22">Last 22 hours</MenuItem>
+                <MenuItem value="23">Last 23 hours</MenuItem>
+                <MenuItem value="24">Last 24 hours</MenuItem>
+            </Select>
+        </FormControl>;
+
+        /*<Select
+            autoWidth={true}
+            value={selectedValue}
+            onChange={this.handleChange}>
+            <MenuItem value="5">Last 5 hours</MenuItem>
+            <MenuItem value="6">Last 6 hours</MenuItem>
+            <MenuItem value="7">Last 7 hours</MenuItem>
+            <MenuItem value="8">Last 8 hours</MenuItem>
+            <MenuItem value="9">Last 9 hours</MenuItem>
+            <MenuItem value="10">Last 10 hours</MenuItem>
+            <MenuItem value="11">Last 11 hours</MenuItem>
+            <MenuItem value="12">Last 12 hours</MenuItem>
+            <MenuItem value="13">Last 13 hours</MenuItem>
+            <MenuItem value="14">Last 14 hours</MenuItem>
+            <MenuItem value="15">Last 15 hours</MenuItem>
+            <MenuItem value="16">Last 16 hours</MenuItem>
+            <MenuItem value="17">Last 17 hours</MenuItem>
+            <MenuItem value="18">Last 18 hours</MenuItem>
+            <MenuItem value="19">Last 19 hours</MenuItem>
+            <MenuItem value="20">Last 20 hours</MenuItem>
+            <MenuItem value="21">Last 21 hours</MenuItem>
+            <MenuItem value="22">Last 22 hours</MenuItem>
+            <MenuItem value="23">Last 23 hours</MenuItem>
+            <MenuItem value="24">Last 24 hours</MenuItem>
+        </Select>;*/
     }
 }
 
